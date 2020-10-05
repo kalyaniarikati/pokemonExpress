fba682dfc3a243396f92ce432829d5f5ce710890
Kalyani Arikati <kalyani.arikati@gmail.com>
2020-10-05 23:41:31 +1100

    tests added

--- test/poke.test.js
+++ test/poke.test.js
@@ -7,3 +7,22 @@ describe("get all pokemons",  () =>{
         expect(Object.keys(pokemons).length).not.toBe(0)
     })
 })
+
+describe("get all pokemons",  () =>{
+    test("should get a pokemon if one exists", async() => {
+        let pokemons=await utilities.getAllPokemons({});
+        expect(Object.keys(pokemons).length).not.toBe(0)
+    })
+})
+
+describe("get pokemon by id",  () =>{
+    const req = {
+        params: {
+            id: 6
+        }
+    }
+    test("should get a pokemon if one exists", async() => {
+        let pokemons=await utilities.getPokemonById(req.params.id);
+        expect(pokemons).not.toBeNull();
+    })
+})
